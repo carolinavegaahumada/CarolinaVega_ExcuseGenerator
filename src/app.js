a60009d6e07293407df6c5668ffe7c9440650029
Adriana Carolina Vega Ahumada <carolinavegaahumada@gmail.com>
2023-07-12 16:31:07 +0000

    primera version excuse

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["My baby", "The dog", "The cat"];
+  let action = ["ate", "broke", "peed", "hid"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  document.getElementById("excuse").innerHTML =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
 };
